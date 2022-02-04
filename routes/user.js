ad4d76d83af15f815be158d53955296b494956be
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2022-02-04 02:40:33 -0600

    Bug fixes in users endpoint

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ router.get('/:username',
     async (req, res) => {
         try{
             if(req.user.username == req.params.username)
-                res.redirect('../../me');
+                return res.redirect('/api/2/me');
             let user = await UserService.findUnique({
                 where: {
                     username: req.params.username
@@ -26,7 +26,8 @@ router.get('/:username',
             delete user.email;
             delete user.telephone;
             // Following
-            user.following = req.user && UserService.followsUser({user_id: req.user.user_id, following_user_id: user.user_id})
+            user.following = req.user && await UserService.followsUser({user_id: req.user.user_id, following_user_id: user.user_id})
+            console.log(user.following)
             res.json(user);
         } catch(e){
             console.log(e);
