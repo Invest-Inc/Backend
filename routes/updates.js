e166de880672d9a42c4e84df9110483166624368
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2022-02-05 18:29:50 -0600

    Include full startup object on updates

--- routes/updates.js
+++ routes/updates.js
@@ -9,7 +9,10 @@ router.get('/:startup_update_id',
     async (req, res) => {
         try{
             const data = await StartupUpdateService.findUnique({
-                where: {startup_update_id: parseInt(req.params.startup_update_id)}
+                where: {startup_update_id: parseInt(req.params.startup_update_id)}, 
+                include: {
+                    Startup: true
+                }
             });
             if(req.user != undefined){
                 data.current_user_permissions = await StartupService.getUserPermissions({
